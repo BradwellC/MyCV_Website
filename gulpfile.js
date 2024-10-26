32589fe574626f2867336db4f8b0ec2a252b7a39
Jormungandr <chris-bradwell@live.co.uk>
2024-10-26 09:01:41 +0100

    New SASS Folder Structure & Compiler setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,51 @@
-import { dest, series, src, watch } from 'gulp';
-import babel from 'gulp-babel';
+const { dest, src, watch, series } = require('gulp');
+const babel = require('gulp-babel');
 
-import gulpSass from 'gulp-sass';
-import * as sass from 'sass';
-const scss = gulpSass(sass);
+const sass = require('gulp-sass')(require('sass'));
 
-import autoprefixer from 'gulp-autoprefixer';
-import cssMinfy from 'gulp-clean-css';
+const cssMinfy = require('gulp-clean-css');
 
-import concat from 'gulp-concat';
-import rename from 'gulp-rename';
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+
+const jsMinfy = require('gulp-terser');
+
 
 // CSS Gulp
 
 function styles() {
-  return src('./public/Jormungandr/styles/**/*.scss')
-    .pipe(scss())
-    .pipe(
-      autoprefixer({
-        overrideBrowserslist: ['last 2 versions', '> 1%'], // Adjust the browser support according to your needs
-        cascade: false,
-      })
-    )
-    .pipe(dest('./public/css/'))
+  return src('Library/styles/**/*.{scss, sass}')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('public/css'))
     .pipe(cssMinfy())
     .pipe(
       rename({
         suffix: '.min',
       })
     )
-    .pipe(dest('./public/css/'));
+    .pipe(dest('public/css'));
 }
 
 // JS Gulp
-import jsMinfy from 'gulp-terser';
 
 function scripts() {
-  return src('./public/Jormungandr/scripts/**/*.js')
-    .pipe(babel())
+  return src('Library/scripts/**/*.js')
+    .pipe(babel({presets: ['@babel/preset-env']}))
     .pipe(concat('scripts.js'))
-    .pipe(dest('./public/js'))
+    .pipe(dest('public/js'))
     .pipe(jsMinfy())
     .pipe(
       rename({
         suffix: '.min',
       })
     )
-    .pipe(dest('./public/js'));
+    .pipe(dest('public/js'));
 }
 
 // Watch Gulp
 function watchGulp() {
-  watch(
-    [
-      './public/Jormungandr/styles/**/*.scss',
-      './public/Jormungandr/scripts/**/*.js',
-    ],
-    series(styles, scripts)
-  );
+  watch('Library/scripts/**/*.js', series(scripts));
+  watch('Library/styles/**/*.scss', series(styles));
 }
 
-export default series(styles, scripts, watchGulp);
+exports.default = series(styles, scripts, watchGulp);
